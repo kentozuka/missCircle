39fa0405ecc78d102215946e2b44c8803b1a030f
kento <kentozuka22@gmail.com>
2020-06-08 16:56:56 +0900

    why

--- client/src/main.js
+++ client/src/main.js
@@ -10,7 +10,7 @@ import './assets/css/main.css'
 Vue.config.productionTip = false
 
 // change this
-axios.defaults.baseURL = process.env.baseURL + 'api' || 'http://127.0.0.1:5000/api'
+axios.defaults.baseURL = 'https://miss-circle.herokuapp.com/'
 
 new Vue({
   router,
