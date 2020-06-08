615d7c7e59096f763371fa8363225027761e4edb
kento <kentozuka22@gmail.com>
2020-06-08 16:51:32 +0900

    base url wasureteta

--- client/src/main.js
+++ client/src/main.js
@@ -10,7 +10,7 @@ import './assets/css/main.css'
 Vue.config.productionTip = false
 
 // change this
-axios.defaults.baseURL = 'http://127.0.0.1:5000/api'
+axios.defaults.baseURL = process.env.baseURL + 'api' || 'http://127.0.0.1:5000/api'
 
 new Vue({
   router,
